e00d42cee012be83746384eaf49dc65085a3e367
Vova Petrov <v.petrov3@g.nsu.ru>
2024-11-27 00:03:21 +0700

    add swagger

---
+++ backend/timetable-back/src/main/java/ru/nsu/timetable/configuration/security/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package ru.nsu.timetable.configuration.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Timetable Api",
+                description = "Timetable", version = "1.0.0",
+                contact = @Contact(
+                        name = "NSU-team-3c"
+                )
+        )
+)
+
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+
+public class OpenApiConfig {
+
+}
