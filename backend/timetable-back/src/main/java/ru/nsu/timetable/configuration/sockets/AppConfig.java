dea1b08cd8a38293571f9d5e9fc5d6eb9dca4f6d
d.barsukova <d.barsukova@g.nsu.ru>
2024-11-27 12:34:53 +0700

    update auth

---
+++ backend/timetable-back/src/main/java/ru/nsu/timetable/configuration/sockets/AppConfig.java
@@ -0,0 +1,20 @@
+package ru.nsu.timetable.configuration.sockets;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(false)
+                .allowedHeaders("*");
+    }
+}
